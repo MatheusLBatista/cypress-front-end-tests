6d4e8f22c28976812b2e131f2868ee48d16a5ef4
MatheusLBatista <matheusifro2020@gmail.com>
2025-08-29 20:38:11 -0400

    implementing negative cases while loging

--- cypress/e2e/FormularioLogin.cy.js
+++ cypress/e2e/FormularioLogin.cy.js
@@ -18,14 +18,23 @@ describe("Formulário de cadastro", () => {
     })
 
     it("Não deve permitir um email inválido", () => {
-        
+        // cy.getByData('email-input').type('teste@.com');
+        // cy.getByData('senha-input').type('123456');
+        // cy.getByData('botao-enviar').click();
+        // cy.contains("alert").should('be.visible')
     })
 
     it("Não deve permitir um email em branco", () => {
-
+        cy.getByData('email-input').type('     ');
+        cy.getByData('senha-input').type('123456');
+        cy.getByData('botao-enviar').click();
+        cy.contains("span", 'O campo email é obrigatório').should('be.visible')
     })
 
     it("Deve informar um erro ao tentar logar caso senha ou email estejam errados", () => {
-
+        cy.getByData('email-input').type('testando@gmail.com');
+        cy.getByData('senha-input').type('123456');
+        cy.getByData('botao-enviar').click();
+        cy.contains("span", 'E-mail ou senha incorretos')
     })
 })
